962491c035aab41fdca992a675c93a67c9eb669a
Howard Shen <howardshen2306@gmail.com>
2023-06-22 14:13:26 +0800

    feat: Create heroes controller, service and module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HeroesController } from './heroes/heroes.controller';
+import { HeroesService } from './heroes/heroes.service';
+import { HeroesModule } from './heroes/heroes.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [HeroesModule],
+  controllers: [AppController, HeroesController],
+  providers: [AppService, HeroesService],
 })
 export class AppModule {}
