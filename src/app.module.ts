abbe012e9f1be190befd65180a363edc4eba5e0b
Howard Shen <howardshen2306@gmail.com>
2023-06-23 00:33:14 +0800

    refactor: Remove default AppController and AppService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { HeroesController } from './heroes/heroes.controller';
-import { HeroesService } from './heroes/heroes.service';
 import { HeroesModule } from './heroes/heroes.module';
 
 @Module({
   imports: [HeroesModule],
-  controllers: [AppController, HeroesController],
-  providers: [AppService, HeroesService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
